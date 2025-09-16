babb6274d298cf0479a99984d3c9f47902ccefa2
Gonzalo Perez <gonzalo.anuar13@outlook.com>
2025-09-16 16:39:43 -0600

    Ejemplo con astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({ context: "server", access: "secret" }),
+      SCORE_API_ENDPOINT: envField.string({ context: "server", access: "public" })
+    }
+  },
+
+  adapter: node({
+    mode: "standalone"
+  })
+});
